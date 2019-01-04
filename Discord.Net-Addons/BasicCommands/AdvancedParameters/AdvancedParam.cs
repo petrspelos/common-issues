ca98b57dcb124380ad353213a72f2cb1cea6ced4
Joel Parkinson <joelparkinson@live.co.uk>
2019-01-04 23:14:33 +0000

    Add AdvancedParam.cs

---
+++ Discord.Net-Addons/BasicCommands/AdvancedParameters/AdvancedParam.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Botwyn.Modules
+{
+    public class AdvancedParam : ModuleBase<SocketCommandContext>
+    {
+        [Command("UserInfo")]
+        public async Task UserInfo(SocketGuildUser user)
+        {
+            await ReplyAsync($"The Users Username is: {user.Username}, They Joined On: {user.JoinedAt}");
+        }
+
+        [Command("ChanInfo")]
+        public async Task ChanInfo(SocketGuildChannel channel)
+        {
+            await ReplyAsync($"The channel name is: {channel.Name}, The ID is: {channel.Id}");
+        }
+    }
+}
