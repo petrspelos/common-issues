c362ad1e9fb4e69d9fe79b46beaa47d54a0c5fe1
Joel Parkinson <joelparkinson@live.co.uk>
2019-01-08 16:42:32 +0000

    Add Guide Global Static Client & CommandService

---
+++ Discord.Net-Addons/GlobalStatic/ExampleBot/GlobalStatic_ExampleBot/Modules/Misc.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using GlobalStatic_ExampleBot.Services;
+
+namespace GlobalStatic_ExampleBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+            => await ReplyAsync($"{Global.Client.Latency}");
+    }
+}
---
+++ Discord.Net-Addons/GlobalStatic/ExampleBot/GlobalStatic_ExampleBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using GlobalStatic_ExampleBot.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace GlobalStatic_ExampleBot
+{
+    class Program
+    {
+        static Task Main(string[] args)
+            => new DiscordService().InitializeAsync();
+    }
+}
